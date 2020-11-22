7dbe6c39cf82fb7173b63d5cabc165d756a68506
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-11-22 14:51:58 +0300

    реализация примера

--- main.cpp
+++ main.cpp
@@ -1,145 +1,96 @@
 #include <iostream>
 using namespace std;
 
-//композиция - сам объект
-//агрегация - указатель
-//где странная стрелочка, там и содержиться указатель или объект
-
-class Product {
- public:
-  virtual void print() = 0;
-};
-
-class ConcreteProduct1 : public Product {
- public:
-  void print() override {
-    cout << "ConcreteProduct1" << endl;
-  }
-};
-
-class ConcreteProduct2 : public Product {
- public:
-  void print() override {
-    cout << "ConcreteProduct2" << endl;
-  }
-};
-
-//class Creator{
-// public:
-//   Product* createObj(int id){
-//     switch (id) {
-//       case 1:{
-//         return new ConcreteProduct1();
-//       }
-//       case 2:{
-//         return new ConcreteProduct2();
-//       }
-//       default: {
-//         return nullptr;
-//       }
-//     }
-//   };
-//};
-
-class Room{
- public:
-  virtual void setSize(){}
-};
-
-class SpecialRoom: public Room{
- public:
-  void setSize() override{}
-};
-
 class Wall {
  public:
-  virtual void setDoor(){}
+  //допустим тут вся стена будет одним цветом
+  virtual void setColor(string color) {
+    cout << "I am ordinary " << color << " wall" << endl;
+  }
 };
 
-class Maze{
-
+class SpecialWall : public Wall {
  public:
-   void draw(){}
-   void addRoom(Room* r){}
-   void addWall(Wall* r){}
+  //а тут стена с градиентной заливкой
+  void setColor(string color) override {
+    cout << "I am gradient " << color << " wall" << endl;
+  }
 };
 
-
-class  MazeCreator{
+class Room {
  public:
-  Room* createRoom(){
+  virtual void addSide(Wall *wall) {
+    cout << "I am ordinary room" << endl;
+  }
+};
+
+class SpecialRoom : public Room {
+ public:
+  void addSide(Wall *wall) override {
+    cout << "I am special room" << endl;
+  }
+};
+
+class Maze {
+ public:
+  void addRoom(Room *r) {}
+};
+
+class MazeGame {
+ public:
+
+  //фабричный метод
+  virtual Room *createRoom() {
     return new Room();
   }
-  Wall* createWall(){
+  //фабричный метод
+  virtual Wall *createWall() {
     return new Wall();
   }
 
-  virtual Maze* createMaze(){
-    auto r1 = createRoom();
-    r1->setSize();
-    auto w1 = createWall();
-    Maze* m  = new Maze();
+  virtual Maze *createMaze() {
+    Room *r1 = createRoom();
+    Wall *w1 = createWall();
+    w1->setColor("black");
+    r1->addSide(w1);
+    Maze *m = new Maze();
     m->addRoom(r1);
-    m->addWall(w1);
     return m;
   }
 
 };
 
-class  SpecialMazeCreator: Maze{
+class SpecialMazeGame : public MazeGame {
  public:
-  Room* createRoom(){
+  //фабричный метод
+  Room *createRoom() override {
     return new SpecialRoom();
   }
-  Wall* createWall(){
-    return new Wall();
+  //фабричный метод
+  Wall *createWall() override {
+    return new SpecialWall();
   }
 
-  virtual Maze* createMaze(){
-    auto r1 = createRoom();
-    r1->setSize();
-    auto w1 = createWall();
-    Maze* m  = new Maze();
+  Maze *createMaze() override {
+    Room *r1 = createRoom();
+    Wall *w1 = createWall();
+    w1->setColor("black");
+    r1->addSide(w1);
+    Maze *m = new Maze();
     m->addRoom(r1);
-    m->addWall(w1);
+    return m;
   }
 
-};
-
-
-
-
-class Creator {
- public:
-  virtual Product *createObj() = 0;
-};
-
-class ConcreteCreator1 : public Creator {
- public:
-  Product *createObj() override {
-    return new ConcreteProduct1();
-  }
-};
-
-class ConcreteCreator2 : public Creator {
- public:
-  Product *createObj() override {
-    return new ConcreteProduct2();
-  }
 };
 
 int main() {
-//  Creator creator;
-//  Product* p1 = creator.createObj(1);
-//  Product* p2 = creator.createObj(2);
+  cout << "ordinary maze game" << endl;
+  MazeGame m;
+  m.createMaze();
 
+  cout << "special maze game" << endl;
+  SpecialMazeGame m1;
+  m1.createMaze();
 
-  ConcreteCreator1 c1;
-  ConcreteCreator2 c2;
-  Product *p1 = c1.createObj();
-  Product *p2 = c2.createObj();
-
-  p1->print();
-  p2->print();
   return 0;
 }
